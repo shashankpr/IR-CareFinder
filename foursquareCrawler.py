85a4ee42466cf8101967fcad3a4cd988edafac73
kagtag <798066354@qq.com>
2017-03-17 11:51:40 +0100

    Correct errors about the coordinates

--- foursquareCrawler.py
+++ foursquareCrawler.py
@@ -46,23 +46,34 @@ def getHospitalDetails(coorStr,step=0.1):
     workbook=openpyxl.Workbook(write_only=True)
     worksheet = workbook.create_sheet()
 
-    ######
-    xSeq=[]
-    ySeq=[]
-    xxSeq=[]
-    yySeq=[]
-    ######
+
 
     for i in range(exstep[0]):
         for j in range(exstep[1]):
-            nepoint=str([round(coor[0][0]-i*step,2),round(coor[0][1]-j*step,2)])[1:-2]
-            swpoint=str([round(coor[0][0]-step*(i+1),2),round(coor[0][1]-step*(j+1),2)])[1:-2]
+            ######
+            xSeq = []
+            ySeq = []
+            xxSeq = []
+            yySeq = []
+            ######
+
+
+            nepoint=str([round(coor[0][0]-i*step,2),round(coor[0][1]-j*step,2)])[1:-1]
+            swpoint=str([round(coor[0][0]-step*(i+1),2),round(coor[0][1]-step*(j+1),2)])[1:-1]
             print(nepoint,swpoint)
 
-            xSeq.append(round(coor[0][0]-i*step,2))
-            ySeq.append(round(coor[0][1]-j*step,2))
-            xxSeq.append(round(coor[0][0]-step*(i+1),2))
-            yySeq.append(round(coor[0][1]-step*(j+1),2))
+            #
+            # xSeq.append(round(coor[0][0]-i*step,2))
+            # ySeq.append(round(coor[0][1]-j*step,2))
+            # xxSeq.append(round(coor[0][0]-step*(i+1),2))
+            # yySeq.append(round(coor[0][1]-step*(j+1),2))
+            # plt.scatter([coor[0][0],coor[1][0]],[coor[0][1],coor[1][1]])
+            # plt.xlim(coor[0][0]-0.5,coor[1][0]+0.5)
+            # plt.ylim(coor[0][1]-0.5,coor[1][1]+0.5)
+            # plt.scatter(xSeq, ySeq)
+            # plt.scatter(xxSeq, yySeq)
+            # plt.show()
+            #
 
 
             results=client.venues.search(params={'query':'hospitals','intent':'browse',
@@ -103,9 +114,7 @@ def getHospitalDetails(coorStr,step=0.1):
 
             time.sleep(random.randint(2,3))
     workbook.save("foursquare/data.xlsx")
-    plt.scatter(xSeq,ySeq)
-    plt.scatter(xxSeq,yySeq)
-    plt.show()
+
 
 targetSquare=["40.797480, -73.858479","40.645527, -74.144426"] #new york city
 
