879b400d253f5b2e862b31784a937991315e9f9e
Sylvia Ch <siyuchen0507@gmail.com>
2017-03-28 12:18:10 +0200

    Update doctornames_test.py

--- Code/doctornames_test.py
+++ Code/doctornames_test.py
@@ -1,4 +1,4 @@
 from doctornames import DoctorNames
 
-docname_results = DoctorNames({'hospitalurl' : 'http://www.bronxcare.org/physicians/physician-listing/'})
+docname_results = DoctorNames({'hospitalurl' :'http://www.bronxcare.org/physicians/physician-listing/'})
 docname_results.execute()
