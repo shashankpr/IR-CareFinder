71124a49372168c1b349284e23bd2d556addd28f
Shashank <shashank1692@gmail.com>
2017-03-28 11:47:40 +0200

    Added extraction from abstract list

--- keyword_extraction/kw_extractor/kw_extractor.py
+++ keyword_extraction/kw_extractor/kw_extractor.py
@@ -10,15 +10,23 @@ class extractor:
         self.minNoOfWords       =   3       # Each keyword appears in the text at least 3 times
 
     def rake_extract(self):
-        abstractFile        =   self.abstractFile
-        noOfCharacters      =   self.noOfCharacters
-        maxNoOfWords        =   self.minNoOfWords
-        minNoOfWords        =   self.minNoOfWords
+        abstractFile    =   self.abstractFile
+        noOfCharacters  =   self.noOfCharacters
+        maxNoOfWords    =   self.minNoOfWords
+        minNoOfWords    =   self.minNoOfWords
+
+        keyword_list   =   []
 
         rake_object     =   rake.Rake("data/stoplists/SmartStoplist.txt", noOfCharacters, maxNoOfWords, minNoOfWords)
 
+        # If test_file is a list containing pubmed abstracts
+        # for abstracts in abstractFile:
+        #     keywords    =   rake_object.run(abstracts)
+        #     keyword_list.append(keywords)
+
         keywords        =   rake_object.run(abstractFile)
         print "Keywords: ", keywords
+
 
 
 test_file       =   open("data/testData/pubmedtest.txt", 'r')
