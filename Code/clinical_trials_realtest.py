5643661d94cdd01bb1b77d0d8632560af8c19d60
Jens Voortman <racingdutch@hotmail.com>
2017-03-28 12:38:23 +0200

    Working real world tests

--- Code/clinical_trials_realtest.py
+++ Code/clinical_trials_realtest.py
@@ -2,11 +2,17 @@ from clinical_trials_crawler import ClinicalTrialsCrawler
 import pytest
 
 
-def test_realworld():
-    with_results = ClinicalTrialsCrawler({'search': 'Roosevelt Hospital New York'})
+def pytest_generate_tests(metafunc):
+    if "search_string" in metafunc.funcargnames:
+        with open('list_of_hospitals.txt') as f:
+            hospitals = f.readlines()
+            metafunc.parametrize("search_string", hospitals)
+
+def test_realworld(search_string):
+    print search_string
+    with_results = ClinicalTrialsCrawler({'search': search_string})
     with_results.execute()
     with_results.print_count()
-    assert (with_results.get_amount_of_results() > 0)
     assert (with_results.get_amount_of_results() == with_results.get_downloaded())
 
 
