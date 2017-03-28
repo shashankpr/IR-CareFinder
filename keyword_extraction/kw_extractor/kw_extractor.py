6ddd076c7fff5e9b04edd2e14f65e097c15eadb3
Shashank <shashank1692@gmail.com>
2017-03-28 11:30:54 +0200

    RAKE implemention for Keyword extraction

---
+++ keyword_extraction/kw_extractor/kw_extractor.py
@@ -0,0 +1,27 @@
+import rake
+import operator
+
+
+class extractor:
+    def __init__(self, abstractFile):
+        self.abstractFile       =   abstractFile
+        self.noOfCharacters     =   5       # Each word has at least 5 characters
+        self.maxNoOfWords       =   3       # Each phrase has at most 3 words
+        self.minNoOfWords       =   3       # Each keyword appears in the text at least 3 times
+
+    def rake_extract(self):
+        abstractFile        =   self.abstractFile
+        noOfCharacters      =   self.noOfCharacters
+        maxNoOfWords        =   self.minNoOfWords
+        minNoOfWords        =   self.minNoOfWords
+
+        rake_object     =   rake.Rake("data/stoplists/SmartStoplist.txt", noOfCharacters, maxNoOfWords, minNoOfWords)
+
+        keywords        =   rake_object.run(abstractFile)
+        print "Keywords: ", keywords
+
+
+test_file       =   open("data/testData/pubmedtest.txt", 'r')
+text            =   test_file.read()
+extractorObject =   extractor(text)
+keywords        =   extractorObject.rake_extract()
