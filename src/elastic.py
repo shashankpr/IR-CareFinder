136fc8803954a0174fd54fda4d213450e920640e
Wouter van der Zwan <mail@wvdzwan.nl>
2017-04-10 18:06:28 +0200

    Escape elasticsearch query
    Also add missing nltk package to readme

--- src/elastic.py
+++ src/elastic.py
@@ -8,6 +8,50 @@ elastic = Elasticsearch(
     http_auth=(settings['elastic']['user'], settings['elastic']['pass'])
 )
 
+############################
+## http://opensourceconnections.com/blog/2013/01/17/escaping-solr-query-characters-in-python/
+############################
+
+# These rules all independent, order of
+# escaping doesn't matter
+escapeRules = {'+': r'\+',
+               '-': r'\-',
+               '&': r'\&',
+               '|': r'\|',
+               '!': r'\!',
+               '(': r'\(',
+               ')': r'\)',
+               '{': r'\{',
+               '}': r'\}',
+               '[': r'\[',
+               ']': r'\]',
+               '^': r'\^',
+               '~': r'\~',
+               '*': r'\*',
+               '?': r'\?',
+               ':': r'\:',
+               '"': r'\"',
+               ';': r'\;',
+               ' ': r'\ '}
+
+def escapedSeq(term):
+    """ Yield the next string based on the
+        next character (either this char
+        or escaped version """
+    for char in term:
+        if char in escapeRules.keys():
+            yield escapeRules[char]
+        else:
+            yield char
+
+def escapeSolrArg(term):
+    """ Apply escaping to the passed in query terms
+        escaping special characters like : , etc"""
+    term = term.replace('\\', r'\\')   # escape \ first
+    return "".join([nextStr for nextStr in escapedSeq(term)])
+
+###############################
+
 
 def get_hospitals_by_normalized_name(normalized_name):
     search_query = {
@@ -17,7 +61,7 @@ def get_hospitals_by_normalized_name(normalized_name):
                     {
                         "query_string": {
                             "default_field": "normalized-name.keyword",
-                            "query": normalized_name
+                            "query": escapedSeq(normalized_name)
                         }
                     }
                 ]
