eacdc28685db52a24827b26838515ca8c70c4a57
shashankpr <s.p.rao@student.tudelft.nl>
2017-04-12 15:28:06 +0200

    Querying on regex

--- src/semantic_kw.py
+++ src/semantic_kw.py
@@ -1,6 +1,9 @@
 from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient import client
 import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 gdb = GraphDatabase("http://localhost:7474", username='neo4j', password='test123')
 
@@ -8,22 +11,31 @@ gdb = GraphDatabase("http://localhost:7474", username='neo4j', password='test123
 def query_cypher(keyword):
 
     processed_kw = process_keyword(keyword)
-    q1 = 'match (i)-[:RELATED_TO]->(r)'
-    q2 = 'match (i)-[:ALSO_RELATED_TO]->(ar)'
-    q3 = 'where i.name=' + processed_kw
-    q4 = ' return i, r, ar'
-    q = q1 + q2 + q3 + q4
+    q1 = 'MATCH (i)-[:RELATED_TO]->(r)'
+    q2 = 'MATCH (i)-[:KW_RELATED]->(ar)'
+    q3 = 'MATCH (r)-[:KW_RELATED]->(ar)'
+    q4 = 'MATCH (i)-[:TYPE_OF]->(t)'
+    q5 = 'WHERE i.name=~' + processed_kw  + ' or r.name=~' + processed_kw
+    q6 = ' RETURN i, r, ar, t'
+    q = q1 + q2 + q3 + q4 + q5 + q6
 
-    results = gdb.query(q, returns=(client.Node, client.Node, client.Node))
+    results = gdb.query(q, returns=(client.Node, client.Node, client.Node, client.Node))
+    semantic_dict = {}
 
     for i in results:
-        print i[0]['name'], i[1]['name'], i[2]['name']
-    return results
+        semantic_dict['main_query']      = i[0]['name']
+        semantic_dict['main_related']    = i[1]['name']
+        semantic_dict['related_kw_list'] = i[2]['name']
+        semantic_dict['illness_type']    = i[3]['name']
+
+    logging.info(semantic_dict)
+    return semantic_dict
 
 def process_keyword(keyword):
     kw = str(keyword)
     double_q = '"'
-    processed_kw = double_q + kw + double_q
+    regex = '.*'
+    processed_kw = double_q + kw + regex + double_q
 
     return processed_kw
 
